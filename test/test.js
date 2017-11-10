8a86d4480979450d9aceab7736145e3529b01238
Juan Benavides Romero <juan@altairstudios.es>
2017-11-10 19:02:15 +0100

    coveralls with tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,14 @@
+/*global describe it before after*/
 describe('Test', function() {
 	describe('Dummy', function() {
 		it('Its ok', function(done) {
 			done();
+		});
+
+		it('Its ok method', function(done) {
+			require('../routes/web/listener')({}, { send: () => {
+				done();
+			}});
 		});
 	});
 });
