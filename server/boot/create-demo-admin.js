55b0bd645295611631a373ade0780e5c328b4827
Juan Benavides Romero <jbalde@gmail.com>
2017-11-27 01:40:09 +0100

    only create a demo admin if no users

--- server/boot/create-demo-admin.js
+++ server/boot/create-demo-admin.js
@@ -1,7 +1,12 @@
 'use strict';
 module.exports = (app) => {
 	const User = app.models.User;
-	User.create({email: 'admin@douscrm.com', password: 'admin'}, (err, userInstance) => {
-		console.log(userInstance);
+
+	User.count((err, count) => {
+		if(count == 0) {
+			User.create({email: 'admin@douscrm.com', password: 'admin'}, (err, userInstance) => {
+				console.log(userInstance);
+			});
+		}
 	});
 };
