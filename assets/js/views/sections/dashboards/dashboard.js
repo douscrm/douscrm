d4a87d08e13ea03cca0d67f3ab5d908afb430c8e
Juan Benavides Romero <jbalde@gmail.com>
2017-12-02 13:34:25 +0100

    Refactor task list with antd interface

--- assets/js/views/sections/dashboards/dashboard.js
+++ assets/js/views/sections/dashboards/dashboard.js
@@ -13,142 +13,14 @@ import {TodayTasksProgress, FutureTasksProgress, DelayTasksProgress} from '../..
 class Dashboard extends Component {
 	constructor(props) {
 		super(props);
-		/*
-		this.state = {
-			loadingScreen: true,
-			modelLoad: false,
-			refreshId: '0',
-			delayTaskLoad: false,
-			currentTaskLoad: false,
-			futureTaskLoad: false
-		};
-
-		this.models = [];
-
-		this.taskData = {
-			current: {},
-			dealy: {},
-			future: {}
-		};
-
-		this.completeTask = this.completeTask.bind(this);
-		this.getDelayTask = this.getDelayTask.bind(this);
-		this.getCurrentTask = this.getCurrentTask.bind(this);
-		this.getFutureTask = this.getFutureTask.bind(this);*/
 	}
 
 
 
 	componentDidMount() {
-		/*const self = this;
-		const id = self.props.match.params.id;
-
-		setTimeout(() => {
-			self.setState({ loadingScreen: false });
-		}, 750);
-
-		this.getDelayTask();
-		this.getCurrentTask();
-		this.getFutureTask();*/
 	}
 
 
-/*
-	getDelayTask() {
-		console.log('logggg')
-		const self = this;
-		axios.get(`${globals.api}/tasks`, {
-			params: {
-				filter: {
-					where: {
-						endDate: {
-							lt: '2017-11-30'
-						}
-					}
-				}
-			}
-		}).then((response) => {
-			self.models = response.data;
-			self.setState({ delayTaskLoad: true });
-		}).catch((error) => {
-			console.log(error)
-		});
-	}
-
-
-
-	getCurrentTask() {
-		const self = this;
-		axios.get(`${globals.api}/tasks`, {
-			params: {
-				filter: {
-					where: {
-						endDate: moment().format('YYYY-MM-DD')
-					}
-				}
-			}
-		}).then((response) => {
-			self.taskData.current.count = 0;
-			self.taskData.current.total = response.data.length;
-			self.taskData.current.data = response.data;
-			
-			_.each(response.data, (item) => {
-				if(item.status == 'completed') {
-					self.taskData.current.count = self.taskData.current.count + 1;
-				}
-			});
-
-			self.taskData.current.percent = parseInt(self.taskData.current.count / self.taskData.current.total);
-
-			if(isNaN(self.taskData.current.percent)) {
-				self.taskData.current.percent = 100;
-			}
-
-			console.log('asdfasdf')
-			console.log(self.taskData)
-
-			self.setState({ currentTaskLoad: true });
-		}).catch((error) => {
-			console.log(error)
-		});
-	}
-
-
-
-	getFutureTask() {
-		const self = this;
-		axios.get(`${globals.api}/tasks`, {
-			params: {
-				filter: {
-					where: {
-						endDate: {
-							gt: moment().format('YYYY-MM-DD')
-						}
-					}
-				}
-			}
-		}).then((response) => {
-			self.models = response.data;
-			self.setState({ futureTaskLoad: true });
-		}).catch((error) => {
-			console.log(error)
-		});
-	}
-
-
-
-	completeTask(model, index) {
-		const self = this;
-
-		axios.patch(`${globals.api}/tasks/${model.id}`, { status: 'completed' }).then((response) => {
-			if(response.status == 200 && response.data.id) {
-				self.models[index].status = 'completed';
-				self.setState({refreshId: _.uniqueId('refresh')});
-			}
-		});
-	}
-*/
-	
 
 	render() {
 		return (
--- assets/js/views/sections/tasks/list.js
+++ assets/js/views/sections/tasks/list.js
@@ -1,9 +1,10 @@
 import React, {Component} from "react";
 import {Link} from 'react-router-dom';
 import _ from 'lodash';
-import Card from '../../components/card';
 import axios from 'axios';
+import moment from 'moment';
 import globals from '../../../globals';
+import { Badge, Breadcrumb, Table, Layout, Row, Col, Card, Button, Icon } from 'antd';
 
 
 
@@ -13,23 +14,30 @@ class TaskList extends Component {
 
 		this.state = {
 			modelLoad: false,
-			refreshId: '0'
+			refreshId: '0',
+			loading: true
 		};
 
 		this.models = [];
 
-		this.completeTask = this.completeTask.bind(this);
+		this.handleTableChange = this.handleTableChange.bind(this);
+		this.statusTask = this.statusTask.bind(this);
+		this.callTasks = this.callTasks.bind(this);
 	}
 
 
 
 	componentDidMount() {
-		const self = this;
-		const id = self.props.match.params.id;
+		this.callTasks();
+	}
 
+
+
+	callTasks() {
+		const self = this;
 		axios.get(`${globals.api}/tasks`).then((response) => {
 			self.models = response.data;
-			self.setState({ modelLoad: true });
+			self.setState({ loading: false });
 		}).catch((error) => {
 			console.log(error)
 		});
@@ -37,20 +45,157 @@ class TaskList extends Component {
 
 
 
-	completeTask(model, index) {
+	statusTask(model, status) {
 		const self = this;
+		self.setState({ loading: true });
 
-		axios.patch(`${globals.api}/tasks/${model.id}`, { status: 'completed' }).then((response) => {
-			if(response.status == 200 && response.data.id) {
-				self.models[index].status = 'completed';
-				self.setState({refreshId: _.uniqueId('refresh')});
-			}
+		axios.patch(`${globals.api}/tasks/${model.id}`, { status: status }).then((response) => {
+			self.callTasks();
+		}).catch(() => {
+			self.callTasks();
 		});
+	}
+
+
+
+	handleTableChange(pagination, filters, sorter) {
+		this.models = _.sortBy(this.models, [(model)=> {
+			const value = model[sorter.columnKey];
+
+			if(value) {
+				return value.toLowerCase();
+			} else {
+				return value;
+			}
+		}]);
+
+		if(sorter.order != 'descend') {
+			this.models = this.models.reverse();
+		}
+
+		this.setState({refreshId: _.uniqueId('refresh')});
 	}
 
 	
 
 	render() {
+		const columns = [{
+			title: 'Name',
+			dataIndex: 'name',
+			sorter: true,
+			render: (name, model) => {
+				let badge = 'processing'
+				if(model.status == 'completed') {
+					badge = 'success';
+				}
+
+				return (<Link to={`/tasks/${model.id}`}>
+					<Badge status={badge} />
+					{name}
+				</Link>);
+			}
+		}, {
+			title: 'Date',
+			dataIndex: 'endDate',
+			sorter: true,
+			render: (date) => { if(date) { return moment(date).format('YYYY-MM-DD') } else { return null } },
+			filters: [{
+				text: 'Today',
+				value: 'today',
+			}, {
+				text: 'Delayed',
+				value: 'delayed',
+			}, {
+				text: 'Future',
+				value: 'future',
+			}, {
+				text: 'No date',
+				value: 'nodate',
+			}],
+			onFilter: (value, record) => {
+				const date = moment(record.endDate).format('YYYY-MM-DD');
+				const currentDate = moment().format('YYYY-MM-DD');
+
+				console.log(record.endDate)
+
+				return ((value == 'today' && date == currentDate && record.endDate != undefined) || (value == 'delayed' && date < currentDate) || (value == 'future' && date > currentDate) || (value == 'nodate' && record.endDate == null));
+			}
+		}, {
+			title: 'Status',
+			dataIndex: 'status',
+			filters: [{
+				text: 'completed',
+				value: 'completed',
+			}, {
+				text: 'pending',
+				value: 'pending',
+			}, {
+				text: 'progress',
+				value: 'progress',
+			}, {
+				text: 'archived',
+				value: 'archived',
+			}],
+			onFilter: (value, record) => record.status == value
+		}, , {
+			title: 'Action',
+			render: (record) => {
+				let status = null;
+
+				if(record.status == 'completed') {
+					status = (<a href="#" onClick={(event) => {
+						this.statusTask(record, 'pending');
+					}}><Icon type="exclamation-circle-o" style={{ fontSize: 16 }} /></a>);
+				} else {
+					status = (<a href="#" onClick={(event) => {
+						this.statusTask(record, 'completed');
+					}}><Icon type="check-circle-o" style={{ fontSize: 16 }} /></a>);
+				}
+
+				return (<div>{status}</div>);
+			},
+		}];
+
+
+		return (<Layout>
+			<Row>
+				<Col xs={12} sm={12} md={12} lg={12} xl={12}>
+					<Breadcrumb className="dous-breadcrumb">
+						<Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
+						<Breadcrumb.Item>Tasks</Breadcrumb.Item>
+					</Breadcrumb>
+				</Col>
+				<Col xs={12} sm={12} md={12} lg={12} xl={12} className="text-right">
+					<Button type="primary">
+						<Link to={`/tasks/create`}><Icon type="plus" /> Add task</Link>
+					</Button>
+				</Col>
+			</Row>
+			<Row gutter={8} type="flex" justify="space-around" align="middle">
+				<Col xs={24} sm={24} md={24} lg={24} xl={24}>
+					<Card title='Tasks' style={{textAlign: 'center'}}>
+						<Table columns={columns}
+							rowKey={record => record.id}
+							dataSource={this.models}
+							pagination={10}
+							loading={this.state.loading}
+							onChange={this.handleTableChange}
+							expandedRowRender={(record) => <p className="dous-linejump">{record.description}</p>}
+							locale={{
+								filterTitle: 'Title',
+								filterConfirm: 'OK',
+								filterReset: 'Reset',
+								emptyText: 'Not results',
+							}}
+						/>
+					</Card>
+				</Col>
+			</Row>
+		</Layout>);
+
+
+
+
 		const orderedModels = _.sortBy(this.models, [(model)=> {
 			const status = (model.status == 'completed') ? '100' : '999';
 			const date = (model.endDate) ? model.endDate : 0;
