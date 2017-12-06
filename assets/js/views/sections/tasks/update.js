264abb952c3a0511145a70d7a96022b024ed0a21
Juan Benavides Romero <jbalde@gmail.com>
2017-12-06 13:08:52 +0100

    update task refactor

--- assets/js/views/sections/tasks/update.js
+++ assets/js/views/sections/tasks/update.js
@@ -1,9 +1,10 @@
 import React, {Component} from "react";
 import {Link, Redirect} from 'react-router-dom';
-import Card from '../../components/card';
 import axios from 'axios';
 import _ from 'lodash';
+import moment from 'moment';
 import globals from '../../../globals';
+import { Breadcrumb, Layout, Row, Col, Card, Button, Icon, Input, Form, DatePicker } from 'antd';
 
 
 
@@ -12,7 +13,6 @@ class TaskUpdate extends Component {
 		super(props);
 
 		this.state = {
-			name: '',
 			id: null,
 			refreshId: '0'
 		};
@@ -20,6 +20,8 @@ class TaskUpdate extends Component {
 		this.model = {};
 
 		this.onSubmit = this.onSubmit.bind(this);
+		this.changeInputValue = this.changeInputValue.bind(this);
+		this.changeDateValue = this.changeDateValue.bind(this);
 	}
 
 
@@ -32,7 +34,7 @@ class TaskUpdate extends Component {
 			name: this.name
 		}).then((response) => {
 			self.model = response.data;
-			self.setState({ modelLoad: true });
+			self.setState({ id: id });
 		}).catch((error) => {
 			console.log(error)
 		});
@@ -53,52 +55,69 @@ class TaskUpdate extends Component {
 
 	
 
+	changeInputValue(field, value) {
+		this.model[field] = value;
+		this.setState({refreshId: _.uniqueId('refresh')});
+	}
+
+
+
+	changeDateValue(field, date) {
+		this.model[field] = date;
+		this.setState({refreshId: _.uniqueId('refresh')});
+	}
+
+	
+
 	render() {
-		if(this.state.id) {
-			return (<Redirect to={`/tasks/${this.state.id}`}/>);
-		}
+		const formItemLayout = {
+			labelCol: {
+				xs: { span: 24 },
+				sm: { span: 4 },
+			},
+			wrapperCol: {
+				xs: { span: 24 },
+				sm: { span: 20 },
+			}
+		};
 
-		//TODO change to ref inputs see: https://reactjs.org/docs/uncontrolled-components.html
+		console.log('###')
+		console.log(this.model)
+		console.log('###')
 
-		return (<div className="container">
-			<div className="row mt-3">
-				<div className="col-12">
-					<nav aria-label="breadcrumb" role="navigation">
-						<ol className="breadcrumb">
-							<li className="breadcrumb-item">
-								<Link to="/">Home</Link>
-							</li>
-							<li className="breadcrumb-item">
-								<Link to="/tasks">Tasks</Link>
-							</li>
-							<li className="breadcrumb-item">
-								<Link to={`/tasks/${this.model.id}`}>{this.model.name}</Link>
-							</li>
-							<li className="breadcrumb-item active">Add new task</li>
-						</ol>
-					</nav>
-				</div>
-			</div>
-			<div className="row">
-				<div className="col-12">
-					<Card title="Add task" padding={true}>
-						<form onSubmit={this.onSubmit}>
-							<label htmlFor="dous-name">Name</label>
-							<input type="text" className="form-control" id="dous-name" placeholder="Name" required value={this.model.name || ''} onChange={(event) => { this.model.name = event.target.value; }} />
-							<label htmlFor="dous-enddate">End date</label>
-							<input type="date" className="form-control" id="dous-enddate" placeholder="Date" value={(this.model.endDate) ? this.model.endDate.substr(0,10) : ''} onChange={(event) => { this.model.endDate = event.target.value; }} />
-							<label htmlFor="dous-description">Description</label>
-							<textarea className="form-control" id="dous-description" placeholder="Description" value={this.model.description} onChange={(event) => {
-									this.model.description = event.target.value;
-									this.setState({refreshId: _.uniqueId('refresh')});
-								}}></textarea>
-							<br/>
-							<button type="submit" className="btn btn-primary">Update Task</button>
-						</form>
+		const endDate = (this.model.endDate) ? moment(this.model.endDate) : undefined;
+
+		return (<Layout>
+			<Row>
+				<Col xs={12} sm={12} md={12} lg={12} xl={12}>
+					<Breadcrumb className="dous-breadcrumb">
+						<Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
+						<Breadcrumb.Item><Link to="/tasks">Tasks</Link></Breadcrumb.Item>
+						<Breadcrumb.Item>Add new task</Breadcrumb.Item>
+					</Breadcrumb>
+				</Col>
+			</Row>
+			<Row gutter={8}>
+				<Col xs={24} sm={24} md={24} lg={24} xl={24}>
+					<Card title="Add new task" style={{textAlign: 'center'}}>
+						<Form layout="horizontal" onSubmit={this.onSubmit}>
+							<Form.Item label="Name" {...formItemLayout}>
+								<Input placeholder="Task name" required value={this.model.name} onChange={(event) => { this.changeInputValue('name', event.target.value); }} />
+							</Form.Item>
+							<Form.Item label="Due Date" {...formItemLayout} style={{textAlign: 'left'}}>
+								<DatePicker value={endDate} onChange={(date, datestring) => { this.changeDateValue('endDate', date); }} />
+							</Form.Item>
+							<Form.Item label="Description" {...formItemLayout}>
+								<Input.TextArea placeholder="Task description" autosize value={this.model.description} onChange={(event) => { this.changeInputValue('description', event.target.value); }} />
+							</Form.Item>
+							<Form.Item>
+								<Button type="primary" htmlType="submit">Add Task</Button>
+							</Form.Item>
+						</Form>
 					</Card>
-				</div>
-			</div>
-		</div>);
+				</Col>
+			</Row>
+		</Layout>);
 	}
 }
 
