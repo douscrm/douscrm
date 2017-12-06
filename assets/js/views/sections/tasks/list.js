3f0019ced960f9eaa53e8317e645f435123c8b36
Juan Benavides Romero <jbalde@gmail.com>
2017-12-06 01:53:55 +0100

    delete task list legacy code

--- assets/js/views/sections/tasks/list.js
+++ assets/js/views/sections/tasks/list.js
@@ -187,9 +187,6 @@ class TaskList extends Component {
 	
 
 	render() {
-		
-
-
 		return (<Layout>
 			<Row>
 				<Col xs={12} sm={12} md={12} lg={12} xl={12}>
@@ -245,70 +242,6 @@ class TaskList extends Component {
 				</Col>
 			</Row>
 		</Layout>);
-
-
-
-
-		const orderedModels = _.sortBy(this.models, [(model)=> {
-			const status = (model.status == 'completed') ? '100' : '999';
-			const date = (model.endDate) ? model.endDate : 0;
-
-			return `${status}-${date}`;
-		}]);
-
-		const lines = [];
-		for (let i = orderedModels.length - 1; i >= 0; i--) {
-			const model = orderedModels[i];
-			const key = _.uniqueId('task-');
-			const index = i;
-			const icon = (model.status == 'completed') ? 'fa-check-circle-o' : 'fa-circle-o';
-			const className = (model.status == 'completed') ? 'table-dark' : '';
-
-			lines.push(<tr key={key} className={className}>
-				<td ><Link to={`/tasks/${model.id}`}>{model.name}</Link></td>
-				<td >{(model.endDate) ? model.endDate.substr(0,10) : ''}</td>
-				<td >
-					<button type="button" className="btn btn-primary" onClick={(event) => {this.completeTask(model, index)}}>
-						<i className={`fa ${icon}`} />
-					</button>
-				</td>
-			</tr>);
-		}
-
-
-		return (<div className="container">
-			<div className="row mt-3">
-				<div className="col-12">
-					<nav aria-label="breadcrumb" role="navigation">
-						<ol className="breadcrumb">
-							<li className="breadcrumb-item">
-								<Link to="/">Home</Link>
-							</li>
-							<li className="breadcrumb-item active">Tasks</li>
-						</ol>
-					</nav>
-				</div>
-			</div>
-			<div className="row">
-				<div className="col-12">
-					<Card title="Task list">
-						<table className="table">
-							<thead>
-								<tr>
-									<th scope="col">Task</th>
-									<th scope="col">Date</th>
-									<th scope="col">Status</th>
-								</tr>
-							</thead>
-							<tbody>{lines}</tbody>
-						</table>
-						<div className="card-footer text-muted">
-							<Link className="btn btn-primary" to={`/tasks/create`}>Add task</Link>
-						</div>
-					</Card>
-				</div>
-			</div>
-		</div>);
 	}
 }
 
